88cc1fc05f0104d77afd673afa5fc21fa360bf7d
xiejun <xj930312>
2020-11-05 16:18:33 +0800

    up

--- restclient/src/main/java/demo/es/restclient/annotation/ESDocument.java
+++ restclient/src/main/java/demo/es/restclient/annotation/ESDocument.java
@@ -7,10 +7,17 @@ import java.lang.annotation.Target;
 import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
+/**
+ * es文档注解
+ * es6以后，强制一个index下只能有一个type
+ * es以后，将完全移除type
+ */
 @Documented
 @Target(TYPE)
 @Retention(RUNTIME)
 public @interface ESDocument {
+
+    String indexName() default "";//索引名称，必须小写
 
     String typeName() default "";//数据类型
 
